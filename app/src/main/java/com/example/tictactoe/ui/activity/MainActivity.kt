0ceebf293ad8807ce92f23e50375756f00b31ac5
diasjakupov <dsjakupov@gmail.com>
2021-08-18 16:46:44 +0600

    commit 2:setup general settings

--- app/src/main/java/com/example/tictactoe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tictactoe
+package com.example.tictactoe.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
