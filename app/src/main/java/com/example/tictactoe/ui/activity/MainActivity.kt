b9d2e9b015d2c64b698b7630fdfb1c00a6b3d6b1
diasjakupov <dsjakupov@gmail.com>
2021-08-19 17:00:22 +0600

    commit 3:design menu fragment

--- app/src/main/java/com/example/tictactoe/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/ui/activity/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.tictactoe.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.example.tictactoe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main)
     }
 }
